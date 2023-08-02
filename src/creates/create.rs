b8f26cb7350fcf38385d2b7095dff5d32ae9d9ea
Toweave <Toweave@163.com>
2023-08-02 16:39:13 +0800

    add colot

--- src/creates/create.rs
+++ src/creates/create.rs
@@ -1,12 +1,13 @@
 // use std::error::Error;
 use clap::{Subcommand, ValueEnum};
 use crate::structs::root::Args;
+use nu_ansi_term::Color::{Blue, Cyan, Yellow};
 
 #[derive(Debug, Subcommand)]
 pub enum Creates {
     /// Create file or folder to current path.
     Create {
-        mode: Mode,
+        mode: Option<Mode>,
 
         #[arg(short, long)]
         name: String
@@ -30,11 +31,22 @@ pub fn run (args: &Args){
         Some(Creates::Create { mode, name}) => {
             println!("'myapp add' was used, name is: {:?}", mode);
             match mode {
-                Mode::Folder => {
+                Some(o) => {
                     println!("Folder {}", name);
+                    match o {
+                        Mode::Folder => {
+                            // 创建文件夹
+                            println!("Folder {}", name);
+                        }
+                        Mode::File => {
+                            // 创建文件
+                            println!("File {}", name);
+                        }
+                    }
                 }
-                Mode::File => {
-                    println!("File {}", name);
+                None => {
+                    // 默认创建文件
+                    println!("None Create File {}", Blue.paint(name));
                 }
             }
 
